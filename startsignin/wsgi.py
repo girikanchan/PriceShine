c73de34289e73704f9d322f5dbcde714baca1579
girikanchan <94513166+girikanchan@users.noreply.github.com>
2023-07-08 23:05:22 +0530

    Update wsgi.py

--- startsignin/wsgi.py
+++ startsignin/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startsignin.settings')
 
-application = get_wsgi_application()
+price-shine = get_wsgi_application()
